b690d6c16973ea1e17231698b6ddc391cbb3cd8c
Christopher Kim <chris.kim1990@gmail.com>
2021-02-12 16:10:06 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -31,5 +31,6 @@ def aoh_lookup(aoh, row, key)
 end
 
 def aoh_update(aoh, row, key, new_value)
-  assembled_aoh[row][key] = [new_value]
+  assembled_aoh[row][key]
+  assembled_aoh[row][key] = new_value 
 end
